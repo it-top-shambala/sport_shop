08ec3be8768f34f9f77224a0653a96e73140565e
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-09-02 00:06:23 +0300

    Added some changes

--- create_sportshop.sql
+++ create_sportshop.sql
@@ -56,7 +56,16 @@ CREATE TABLE sales(
             ON UPDATE NO ACTION
 );
 /*GOOD TABLE INSERT*/
+
+CREATE TABLE Company(
+    company_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    company_name VARCHAR(125) NOT NULL,
+    company_location VARCHAR(125) NOT NULL,
+    is_delete_company BOOL DEFAULT FALSE
+);
+
 /*EMPLOYESS CREATE TABLE*/
+
 CREATE TABLE employees(
     employees_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     employees_FirstName VARCHAR(100) NOT NULL,
@@ -66,20 +75,24 @@ CREATE TABLE employees(
     employees_date_of_receipt DATE,
     employees_gender VARCHAR(30) NOT NULL,
     employees_salary INT NOT NULL,
-    is_delete BOOL DEFAULT FALSE
+    employees_company_id INT NOT NULL,
+    is_delete BOOL DEFAULT FALSE,
+    FOREIGN KEY(employees_company_id) REFERENCES Company(company_id)
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
 );
 /*EMPLOYESS CREATE TABLE*/
 
 CREATE TABLE client_phones(
-    client_phones_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    phones_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     phone_number VARCHAR(100) NOT NULL,
-    is_delete BOOL DEFAULT FALSE
+    is_delete_client_phones BOOL DEFAULT FALSE
 );
 CREATE TABLE history_orders(
     history_orders_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     history_order VARCHAR(100) NOT NULL,
     order_date DATE,
-    is_delete BOOL DEFAULT FALSE
+    is_delete_history_orders BOOL DEFAULT FALSE
 );
 CREATE TABLE client(
     client_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -91,11 +104,31 @@ CREATE TABLE client(
     client_history_orders_id INT NOT NULL,
     client_discounts INT NOT NULL,
     is_subscribe BOOL DEFAULT FALSE,
-    is_delete BOOL DEFAULT FALSE,
-    CONSTRAINT FOREIGN KEY (client_phones_id) REFERENCES client_phones(client_phones_id)
+    is_delete_client BOOL DEFAULT FALSE,
+    CONSTRAINT FOREIGN KEY (client_phones_id) REFERENCES client_phones(phones_id)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION,
     CONSTRAINT FOREIGN KEY (client_history_orders_id) REFERENCES history_orders(history_orders_id)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION
 );
+
+CREATE VIEW vw_client
+AS SELECT *
+FROM client
+INNER JOIN history_orders h_orders on client.client_id = h_orders.history_orders_id
+INNER JOIN client_phones client_p on client.client_phones_id = client_p.phones_id
+WHERE client.is_delete_client = FALSE AND h_orders.is_delete_history_orders = FALSE AND client_p.is_delete_client_phones = false;
+
+SELECT phones_id AS id,phone_number AS number
+FROM client_phones ORDER BY phone_number;
+
+
+SELECT * FROM vw_client;
+/*INSERTION FAKE DATA TO client*/
+INSERT INTO client_phones(phone_number)VALUE('54645');
+INSERT INTO history_orders(history_order,order_date) VALUE ('5464545',NOW());
+INSERT INTO client(client_FirstName, client_LastName, client_patronymic, client_email,client_discounts,client_phones_id,client_history_orders_id)
+VALUES('TEST3','TESTGUY3','SOMETESTGUY3','SOMETESTGUY@GMAIL.COM3',5445,1,1);
+
+DELETE FROM client_phones WHERE phones_id = 1;
