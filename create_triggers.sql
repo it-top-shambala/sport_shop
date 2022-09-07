f8f7d4b65358825d5f1c26a5e5c18e0c2e115a88
Bairamov Allaz <67458149+BairamovAllaz@users.noreply.github.com>
2022-09-07 12:42:01 +0300

    Update create_triggers.sql

--- create_triggers.sql
+++ create_triggers.sql
@@ -3,9 +3,9 @@ goods_sales FOR EACH ROW
     INSERT INTO History(log_type_id, goods_manufacturer, buyer_firstname, saler_firstname, sales_price, good_name, sales_date)
         VALUES (
                 (SELECT trigger_type.Id FROM trigger_type WHERE trigger_type.type_name = 'insert'),
-                (SELECT goods_manufacturer_name FROM goods_manufacturer WHERE Id = NEW.saled_goods_id),
-                (SELECT first_name FROM Person WHERE Person.Id = NEW.goods_buyer_id ),
-                (SELECT first_name FROM Person WHERE Person.Id = NEW.goods_seller_id),
+                (SELECT goods_manufacturer_name FROM goods_manufacturer WHERE Id = (SELECT Id FROM goods WHERE Id = NEW.Id)),
+                (SELECT first_name FROM Person WHERE Person.Id = (SELECT Id FROM goods WHERE Id = NEW.Id) ),
+                (SELECT first_name FROM Person WHERE Person.Id = (SELECT Id FROM goods_sales WHERE Id = NEW.Id)),
                 New.sales_price,
                 (SELECT goods_name FROM goods WHERE goods.Id),
                 NEW.sales_date
