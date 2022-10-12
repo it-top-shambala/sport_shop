2fb853fe1bf46fb37a52388154c7e940cbc99916
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2022-10-12 21:38:36 +0500

    Update create_db.sql Add create_triggers.sql

--- create_db.sql
+++ create_db.sql
@@ -1,5 +1,19 @@
 CREATE SCHEMA sport_shop_db;
 
+DROP TABLE table_manufacturers;
+DROP TABLE table_product_types;
+DROP TABLE table_products;
+DROP TABLE table_genders;
+DROP TABLE table_positions;
+DROP TABLE table_employee;
+DROP TABLE table_clients;
+DROP TABLE table_orders;
+DROP TABLE table_deals;
+DROP TABLE table_deals_backup;
+DROP TABLE table_products_archive;
+DROP TABLE table_products_last_item;
+
+
 CREATE TABLE table_manufacturers(
     manufacturer_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     manufacturer TEXT NOT NULL
@@ -42,7 +56,8 @@ CREATE TABLE table_employee(
     position INT NOT NULL,
     FOREIGN KEY (position) REFERENCES table_positions (position_id),
     date_of_employment DATE NOT NULL,
-    salary FLOAT NOT NULL
+    salary FLOAT NOT NULL,
+    is_working BOOL DEFAULT TRUE
 );
 
 CREATE TABLE table_clients(
@@ -61,9 +76,13 @@ CREATE TABLE table_clients(
 CREATE TABLE table_orders(
     order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     client_id INT NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES table_clients (client_id),
+    FOREIGN KEY (client_id) REFERENCES table_clients (client_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
     product_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES table_products(product_id),
+    FOREIGN KEY (product_id) REFERENCES table_products(product_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
     amount INT NOT NULL,
     price FLOAT NOT NULL,
     date_of_purchase DATE NOT NULL
@@ -72,13 +91,61 @@ CREATE TABLE table_orders(
 CREATE TABLE table_deals(
     deal_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     product_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES table_products(product_id),
+    FOREIGN KEY (product_id) REFERENCES table_products(product_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
     client_id INT,
-    FOREIGN KEY (client_id) REFERENCES table_clients (client_id),
+    FOREIGN KEY (client_id) REFERENCES table_clients (client_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
     salesman_id INT NOT NULL,
-    FOREIGN KEY (salesman_id) REFERENCES table_employee (employee_id),
+    FOREIGN KEY (salesman_id) REFERENCES table_employee (employee_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
     amount INT NOT NULL,
     price FLOAT NOT NULL,
     date_of_deal DATE NOT NULL
 );
 
+CREATE TABLE table_deals_backup(
+    deal_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES table_products(product_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    client_id INT,
+    FOREIGN KEY (client_id) REFERENCES table_clients (client_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    salesman_id INT NOT NULL,
+    FOREIGN KEY (salesman_id) REFERENCES table_employee (employee_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    amount INT NOT NULL,
+    price FLOAT NOT NULL,
+    date_of_deal DATE NOT NULL
+);
+
+CREATE TABLE table_products_archive(
+    product_archive_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES table_products(product_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE table_products_last_item(
+    product_last_item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES table_products(product_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+
+CREATE TABLE table_employee_archive(
+    employee_archive_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    employee_id INT NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES table_employee(employee_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
---
+++ create_triggers.sql
@@ -0,0 +1,125 @@
+DELIMITER |
+CREATE TRIGGER trigger_insert_to_deals_backup
+    AFTER INSERT ON table_deals
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO table_deals_backup (product_id, client_id, salesman_id, amount, price, date_of_deal)
+            VALUE (NEW.product_id,NEW.client_id,NEW.salesman_id,NEW.amount,NEW.price,NEW.date_of_deal);
+END|
+
+DELIMITER |
+CREATE TRIGGER trigger_drop_product_to_archive
+    AFTER INSERT ON table_deals
+    FOR EACH ROW
+    BEGIN
+        DECLARE amount_left INT;
+        SELECT amount INTO amount_left FROM table_products WHERE NEW.product_id=product_id;
+        IF amount_left=0 THEN
+        INSERT INTO table_products_archive (product_id) VALUE (NEW.product_id);
+        END IF;
+END|
+
+DELIMITER |
+CREATE TRIGGER trigger_drop_product_to_last_item
+    AFTER INSERT ON table_deals
+    FOR EACH ROW
+    BEGIN
+        DECLARE amount_left INT;
+        SELECT amount INTO amount_left FROM table_products WHERE NEW.product_id=product_id;
+        IF amount_left=1 THEN
+        INSERT INTO table_products_last_item (product_id) VALUE (NEW.product_id);
+        END IF;
+END|
+
+DELIMITER |
+CREATE TRIGGER trigger_insert_client
+    BEFORE INSERT ON table_clients
+    FOR EACH ROW
+    BEGIN
+        SET @exists := EXISTS(SELECT client_id FROM table_clients
+                                               WHERE NEW.first_name=first_name AND
+                                                     NEW.middle_name=middle_name AND
+                                                     NEW.last_name=last_name AND
+                                                     NEW.e_mail=e_mail);
+        IF !@exists THEN
+        INSERT INTO table_clients (first_name, middle_name, last_name, gender, e_mail, phone, discount, is_subscribed)
+            VALUE (NEW.first_name, NEW.middle_name, NEW.last_name, NEW.gender, NEW.e_mail, NEW.phone, NEW.discount, NEW.is_subscribed);
+        END IF;
+END|
+
+DELIMITER |
+CREATE TRIGGER trigger_set_discount_if_50000
+    BEFORE INSERT ON table_deals
+    FOR EACH ROW
+    BEGIN
+        SET @exists := EXISTS(SELECT deal_id FROM table_deals
+                                               WHERE NEW.date_of_deal=date(now())AND
+                                                     NEW.client_id=client_id);
+        IF @exists THEN
+         BEGIN
+            DECLARE done INT DEFAULT FALSE;
+            DECLARE temp INT DEFAULT 0;
+            DECLARE sum INT DEFAULT 0;
+            DECLARE price_cursor CURSOR FOR SELECT price FROM table_deals
+                                               WHERE NEW.date_of_deal=date(now())AND
+                                                     NEW.client_id=client_id;
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+            OPEN price_cursor;
+            read_loop: LOOP
+            FETCH price_cursor INTO temp;
+            IF done THEN
+            LEAVE read_loop;
+            END IF;
+            SELECT sum+temp INTO sum;
+            END LOOP;
+            CLOSE price_cursor;
+
+            IF sum>50000 THEN
+                BEGIN
+                    DECLARE existed_discount INT DEFAULT 0;
+                    SELECT discount INTO existed_discount FROM table_clients WHERE client_id = NEW.client_id;
+                    IF existed_discount<15 THEN
+                        UPDATE table_clients
+                        SET discount=15
+                        WHERE client_id=NEW.client_id;
+                    END IF;
+                END;
+            END IF;
+         END;
+        END IF;
+END|
+
+DELIMITER |
+CREATE TRIGGER trigger_drop_employee_to_archive
+    BEFORE UPDATE ON table_employee
+    FOR EACH ROW
+    BEGIN
+        IF NEW.is_working=FALSE THEN
+        INSERT INTO table_employee_archive (employee_id) VALUE (NEW.employee_id);
+        END IF;
+END|
+
+DELIMITER |
+CREATE TRIGGER trigger_if_product_exists
+    BEFORE INSERT ON table_products
+    FOR EACH ROW
+    BEGIN
+        SET @exists := EXISTS(SELECT product_id FROM table_products
+                                               WHERE NEW.product_name=product_name AND
+                                                     NEW.product_type=product_type AND
+                                                     NEW.manufacturer=manufacturer AND
+                                                     NEW.cost_price=cost_price);
+        IF @exists THEN
+         BEGIN
+             DECLARE existed_amount INT DEFAULT 0;
+             DECLARE new_amount INT DEFAULT 0;
+             SELECT amount INTO existed_amount FROM table_products WHERE product_id=NEW.product_id;
+             SELECT existed_amount+NEW.amount INTO new_amount;
+             UPDATE table_products
+                 SET amount = new_amount
+                 WHERE product_id=NEW.product_id;
+         END;
+        ELSE INSERT INTO table_products (product_name, product_type, manufacturer, amount, cost_price, sell_price)
+             VALUES (NEW.product_name, NEW.product_type, NEW.manufacturer, NEW.amount, NEW.cost_price, NEW.sell_price);
+        END IF;
+END|
